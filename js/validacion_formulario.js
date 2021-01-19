c41cb4504301225ffeccc07a6d9eb9d31570e301
esteban <martin.50.6013@gmail.com>
2021-01-19 15:28:22 -0300

    agregando archivos

---
+++ js/validacion_formulario.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+     
+      var forms = document.getElementsByClassName('needs-validation');
+     
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+  /*
+  const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+*/
